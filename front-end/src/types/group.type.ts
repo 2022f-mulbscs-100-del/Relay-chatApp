d70348faa9e8d380c829c70f9b79e10f56b557e7
zohaib ali <zohaib24a@gmail.com>
2026-02-14 00:05:48 +0500

    minor change

--- front-end/src/types/group.type.ts
+++ front-end/src/types/group.type.ts
@@ -1,7 +1,7 @@
 export interface  Group {
     id?: string;
     groupName: string;
-    memberIds: string[];
+    memberIds: (number | string)[];
     createdBy: string ;
     groupMessages:{ id?: string; senderId: string | number; content: string; createdAt: string | Date, isReadBy?: (string | number)[] }[];
     createdAt: string ;
