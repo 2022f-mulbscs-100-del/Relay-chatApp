6c9feab95e0ed0731e712eb581cc8162cfda8e07
zohaib ali <zohaib24a@gmail.com>
2026-03-31 19:13:34 +0500

    fix/deploy-fix

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -38,7 +38,6 @@ const sequelize = new Sequelize(
     process.env.DB_NAME || 'chatapp',
     process.env.DB_USER || 'root',
     process.env.DB_PASSWORD,
-    // dialect: 'mysql',
     {
         dialect: 'mysql',
         host: process.env.DB_HOST || 'localhost',
